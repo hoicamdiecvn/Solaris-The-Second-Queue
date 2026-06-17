6ba20f24fd5494f24de180ffb9e7909c70115315
JukisYuri <23130092@st.hcmuaf.edu.vn>
2026-06-18 01:25:22 +0700

    ci(commitlint.config): config lại commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-export default { extends: ['@conventional'] };
+export default {
+    extends: ['@conventional'],
+    defaultIgnores: true, 
+    ignores: [
+        (message) => message.startsWith('Merge branch'),
+        (message) => message.startsWith('Merge pull request')
+    ],
+};
