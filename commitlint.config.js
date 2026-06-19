9637f141e4330fced4df0ffd3f0d547f04037348
JukisYuri <23130092@st.hcmuaf.edu.vn>
2026-06-19 23:45:37 +0700

    feat(server): setup api hiển thị dashboard stats

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,1 @@
-export default {
-    extends: ['@conventional'],
-    defaultIgnores: true, 
-    ignores: [
-        (message) => message.startsWith('Merge branch'),
-        (message) => message.startsWith('Merge pull request')
-    ],
-};
+export default { extends: ['@conventional'] };
